51a15b44c00a1cf24025d370dc30867f4c2ce0e4
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-02-27 16:16:22 -0500

    done

--- contact.py
+++ contact.py
@@ -1,41 +1,85 @@
 class Contact:
+  contacts = []
+  next_id = 1
 
-  def __init__(self):
+  def __init__(self, first_name, last_name, email, note):
     """This method should initialize the contact's attributes"""
+    self.first_name = first_name
+    self.last_name = last_name
+    self.email = email
+    self.note = note
+    self.id = Contact.next_id
+    Contact.next_id += 1
+
 
 
   @classmethod
-  def create(cls):
+  def create(cls, first_name, last_name, email, note):
     """This method should call the initializer,
     store the newly created contact, and then return it
     """
+    new_contact = Contact(first_name, last_name, email, note)
+    cls.contacts.append(new_contact)
+    return new_contact
 
   @classmethod
   def all(cls):
     """This method should return all of the existing contacts"""
+all_contacts = []
+for item in cls.contacts:
+ all_contacts.append(item)
+return all_contacts
 
   @classmethod
-  def find(cls):
+  def find(cls, id):
     """ This method should accept an id as an argument
     and return the contact who has that id
     """
+  find_contact = 0
+  for contact in cls.contacts:
+      if contact.id = id:
+          find_contact = contact
+      return find_contact
+  else:
+      return "couldn't fin contact"
 
-  def update(self):
+  def update(self, attribute_change, new_value):
     """ This method should allow you to specify
     1. which of the contact's attributes you want to update
     2. the new value for that attribute
     and then make the appropriate change to the contact
     """
-
-
+    if attribute_change not in ['first_name', 'last_name', 'email', 'note']:
+        return False
+    elif attribute_change == 'first_name':
+        self.first_name = new_value
+    elif attribute_change == 'last_name':
+        self.last_name = new_value
+    elif attribute_change == 'email':
+        self.email = new_value
+    elif attribute_change == 'note':
+        self.note = new_value
+    else:
+        False
   @classmethod
-  def find_by(cls):
+  def find_by(cls, attribute, attribute_value):
     """This method should work similarly to the find method above
     but it should allow you to search for a contact using attributes other than id
     by specifying both the name of the attribute and the value
     eg. searching for 'first_name', 'Betty' should return the first contact named Betty
     """
-
+   for num in range(0, len(cls.contact)):
+       current_contact = cls.contacts [num]
+       if attribute == 'first_name' and current_contact.first_name == attribute_value:
+           return current_contact
+       elif attribute == 'last_name' and current_contact.last_name == attribute_value:
+           return current_contact
+       elif attribute == 'email' and current_contact.email == attribute_value:
+           return current_contact
+       elif attribute == 'note' and current_contact.note == attribute_value:
+           return current_contact
+       else:
+           None
 
   @classmethod
   def delete_all(cls):
@@ -44,12 +88,11 @@ class Contact:
 
   def full_name(self):
     """Returns the full (first and last) name of the contact"""
-
+  return self.first_name + ' ' + self.last_name
 
   def delete(self):
     """This method should delete the contact
     HINT: Check the Array class docs for built-in methods that might be useful here
     """
-
+   Contact.contacts.remove(self)
   # Feel free to add other methods here, if you need them.
-
--- crm.py
+++ crm.py
@@ -2,24 +2,82 @@ from contact import Contact
 
 class CRM:
 
-  # def main_menu(self):
-  #
-  #
-  # def print_main_menu(self):
-  #
-  #
-  # def call_option(self):
-  #
-  #
-  # def add_new_contact(self):
-  #
-  #
-  # def modify_existing_contact(self):
-  #
-  #
-  # def delete_contact(self):
-  #
-  #
-  # def display_all_contacts(self):
-  #
-  # def search_by_attribute(self):
+  def main_menu(self):
+   while True:
+    self.print_main_menu()
+    user_selected = int(input())
+    self.call_option(user_selected)
+
+  def print_main_menu(self):
+   print('[1] Add a new contact')
+  print('[2] Modify an existing contact')
+  print('[3] Delete a contact')
+  print('[4] Display all the contacts')
+  print('[5] Search by attribute')
+  print('[6] Exit')
+  print('Enter a number: ')
+
+
+  def call_option(self):
+    if user_selected == 1:
+     self.add_new_contact()
+    elif user_selected == 2:
+     self.modify_existing_contact()
+    elif user_selected == 3:
+     self.delete_contact()
+    elif user_selected == 4:
+      self.display_all_contacts()
+    elif user_selected == 5:
+       self.search_by_attribute()
+elif user_selected ==6:
+        quit()
+
+
+  def add_new_contact(self):
+   print('Enter First Name: ')
+  first_name = input()
+
+  print('Enter Last Name: ')
+  last_name = input()
+
+  print('Enter Email Address: ')
+  email = input()
+
+  print('Enter a Note: ')
+  note = input()
+ new_contact = Contact.create(first_name, last_name, email, note)
+
+  def modify_existing_contact(self):
+   user_id = int(intput("enter the id to update."))
+   select_contact = Contact.find(user.id)
+
+   print("Select an attribute you want to change")
+   print("1 - First Name")
+   print("2 - Last Name")
+   print("2 - Email")
+   print("4 - Note")
+   attribute = int(input('enter the attribute that is going to change'))
+
+  def delete_contact(self):
+  print("To delete your contact information, please enter your contact information")
+  delete_contact = int(input())
+  current_contact = Contact.get(delete_contact)
+  current_contact.delete_instance()
+  print("contact information has been deleted")
+
+
+  def display_all_contacts(self):
+      for info in Contact.select():
+          print(i.full_name())
+
+  def search_by_attribute(self):
+      print("which attribute would you like to see?")
+      attribute_input = input()
+
+      print("What value are you searching")
+      value_input = input()
+
+      print("This is the information: {}". format(Contact.find_by(attribute_input, value_input)))
+
+      a_crm_app = CRM()
+a_crm_app.main_menu()
